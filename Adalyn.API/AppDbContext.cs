121ff686369015ddc970a6671f6e16b75359640d
adalyn-co <itsadalyn.contact@gmail.com>
2026-08-31 15:33:01 +0300

    Neon bulut veritabani eklendi

--- Adalyn.API/AppDbContext.cs
+++ Adalyn.API/AppDbContext.cs
@@ -8,8 +8,8 @@ namespace Adalyn.API
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            // Veritabanı dosyamızın adını belirliyoruz
-            optionsBuilder.UseSqlite("Data Source=adalyn.db");
+            // C#'ın tam olarak anladığı formatta Neon bağlantısı
+            optionsBuilder.UseNpgsql("Host=ep-muddy-hill-b1y4evjn-pooler.c-5.eu-central-1.aws.neon.tech;Database=neondb;Username=neondb_owner;Password=npg_2pKPkC5uZcjO;SslMode=Require;");
         }
     }
 }
